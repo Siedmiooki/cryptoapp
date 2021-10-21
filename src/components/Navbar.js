f331f8f3b1bd35d248183657065406b8457d74d9
Jan Karpowicz <jan.karpowicz11@gmail.com>
2021-10-21 12:06:30 +0200

    2. routing + basic layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,34 @@
 import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from "@ant-design/icons"
+import icon from "../img/logo.png"
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="navContainer">
+            <div className="logoContainer">
+                <Avatar src={icon} size="large" />
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">CryptoApp</Link>
+                </Typography.Title>
+                {/* <Button className="menuControlContainer">
 
+            </Button> */}
+            </div>
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     );
 }
