0d00e7d4e9609109c4334d096ec65c3e715c2c2e
Jan Karpowicz <jan.karpowicz11@gmail.com>
2021-10-26 17:33:04 +0200

    10. minor fix time + millify

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -22,13 +22,13 @@ const CryptoDetails = () => {
 
     const cryptoDetails = data?.data?.coin;
 
-    const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+    const time = ['24h', '7d', '30d', '1y', '5y'];
 
     if (isFetching) return <Loader />
 
 
     const stats = [
-        { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+        { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price, { precision: 3, lowercase: true, decimalSeparator: "," })}`, icon: <DollarCircleOutlined /> },
         { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
         { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
         { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
